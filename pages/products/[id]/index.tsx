28ba542095350fda0eb898aeaabb4cfa1062e36d
Marco <jonghyeok.lim14@gmail.com>
2022-11-29 20:46:02 +0900

    feat: 컨텐츠 다루기=> editor 만들기

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -1,8 +1,9 @@
-// import ImageGallery from 'react-image-gallery'
-
-import Head from 'next/head'
+import CustomEditor from '@components/Editor'
+import { convertFromRaw, convertToRaw, EditorState } from 'draft-js'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import Carousel from 'nuka-carousel/lib/carousel'
+import { useEffect, useState } from 'react'
 
 const images = [
   {
@@ -33,32 +34,64 @@ const images = [
     original: 'https://picsum.photos/id/1019/1000/600/',
     thumbnail: 'https://picsum.photos/id/1019/250/150/',
   },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+  },
 ]
 
 export default function Products() {
-  // return <ImageGallery items={images} />
+  const [index, setIndex] = useState(0)
+
+  const router = useRouter()
+  const { id: productId } = router.query
+  const [editorState, setEditorState] = useState<EditorState | undefined>(
+    undefined
+  )
+
+  useEffect(() => {
+    if (productId != null) {
+      fetch(`/api/get-product?id=${productId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.items.contents) {
+            setEditorState(
+              EditorState.createWithContent(
+                convertFromRaw(JSON.parse(data.items.contents))
+              )
+            )
+          } else {
+            setEditorState(EditorState.createEmpty())
+          }
+          console.log(data)
+        })
+    }
+  }, [productId])
+
   return (
     <>
-      <Head>
-        <meta
-          property="og:url"
-          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
-        />
-        <meta property="og:type" content="article" />
-        <meta
-          property="og:title"
-          content="When Great Minds Don’t Think Alike"
-        />
-        <meta
-          property="og:description"
-          content="How much does culture influence creative thinking?"
-        />
-        <meta
-          property="og:image"
-          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
-        />
-      </Head>
-      <Carousel animation="fade" autoplay withoutControls wrapAround speed={10}>
+      <Carousel
+        animation="fade"
+        autoplay
+        withoutControls
+        wrapAround
+        speed={10}
+        slideIndex={index}
+      >
         {images.map((item) => (
           <Image
             key={item.original}
@@ -70,6 +103,16 @@ export default function Products() {
           />
         ))}
       </Carousel>
+      <div style={{ display: 'flex' }}>
+        {images.map((item, idx) => (
+          <div key={idx} onClick={() => setIndex(idx)}>
+            <Image src={item.original} alt="image" width={100} height={60} />
+          </div>
+        ))}
+      </div>
+      {editorState != null && (
+        <CustomEditor editorState={editorState} readOnly />
+      )}
     </>
   )
 }
